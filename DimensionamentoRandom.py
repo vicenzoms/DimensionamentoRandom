e297cc3d7a975d9020c133a1f96b1a2d0063767d
vicenzoms <vicenzoms@gmail.com>
2026-05-28 22:16:06 -0300

    Update DimensionamentoRandom.py

--- DimensionamentoRandom.py
+++ DimensionamentoRandom.py
@@ -83,7 +83,7 @@ if not st.session_state.authenticated:
         }}
         
         .login-logo {{
-            max-height: 80px;
+            max-height: 100px;
             width: auto;
             margin-bottom: 15px;
         }}
